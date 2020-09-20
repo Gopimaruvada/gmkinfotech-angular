ff5d5e53d1f7da127b34b546456f569968133e45
Gopimaruvada <gopi.maruvada@gmail.com>
2020-09-20 16:53:49 +0530

    added routing
    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { HowWeWorkComponent } from './how-we-work/how-we-work.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
-
   { path: '', component: HomeComponent  },
- 
-
+  { path: 'how-we-work', component: HowWeWorkComponent  },
+  { path: 'portfolio', component: PortfolioComponent  },
 ];
 
 @NgModule({
