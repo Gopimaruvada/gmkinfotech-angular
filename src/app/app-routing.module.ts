941de7c6ebcac0581620a945c8ba35719d351747
Gopimaruvada <gopi.maruvada@gmail.com>
2020-09-20 16:56:54 +0530

    sss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HowWeWorkComponent } from './how-we-work/how-we-work.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent  },
   { path: 'how-we-work', component: HowWeWorkComponent  },
   { path: 'portfolio', component: PortfolioComponent  },
+  { path: 'Contact', component: ContactComponent  },
 ];
 
 @NgModule({
