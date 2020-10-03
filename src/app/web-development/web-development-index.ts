48fb9f69eef6128bf68da8551fab5ef34ed6e294
Gopi Maruvada <gopi.maruvada@gmail.com>
2020-10-03 17:09:44 +0530

    WEB DEVELOPMENT ROUTING
    <p>api-backend works!</p>

---
+++ src/app/web-development/web-development-index.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wd-index',
+  templateUrl: './web-development-index.html',
+  
+})
+export class WebdevelopmentIndex implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/web-development/web-development.module.ts
+++ src/app/web-development/web-development.module.ts
@@ -5,13 +5,30 @@ import { EcommerceComponent } from './ecommerce/ecommerce.component';
 import { CmsComponent } from './cms/cms.component';
 import { ApiBackendComponent } from './api-backend/api-backend.component';
 import { CustomDevelopmentComponent } from './custom-development/custom-development.component';
-
-
+import { Routes, RouterModule } from '@angular/router';
+import { WebdevelopmentIndex } from './web-development-index';
+export const WEB_ROUTES: Routes = [
+  {
+      path: "", component: WebdevelopmentIndex, children: [
+          
+         
+          { path: "enterprise-solutions", component: EnterpriseSolutionsComponent },
+          { path: "ecommerce", component: EcommerceComponent },
+          { path: "cms", component: CmsComponent },
+          { path: "api-backend", component: ApiBackendComponent },
+          { path: "custom-development", component: CustomDevelopmentComponent },
+      
+      ]
+  },
+];
 
 @NgModule({
-  declarations: [EnterpriseSolutionsComponent, EcommerceComponent, CmsComponent, ApiBackendComponent, CustomDevelopmentComponent],
+  declarations: [EnterpriseSolutionsComponent, EcommerceComponent, CmsComponent, ApiBackendComponent, CustomDevelopmentComponent, WebdevelopmentIndex],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(
+      WEB_ROUTES
+  ),
   ]
 })
 export class WebDevelopmentModule { }
