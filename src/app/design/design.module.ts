79897956fb4af536da9f934821220a566e5390a5
avnishnamdev <avnishnamdev@gmail.com>
2020-10-03 23:05:08 +0530

    update
    update

--- src/app/design/design.module.ts
+++ src/app/design/design.module.ts
@@ -3,13 +3,30 @@ import { CommonModule } from '@angular/common';
 import { WireframeComponent } from './wireframe/wireframe.component';
 import { UxComponent } from './ui/ux/ux.component';
 import { AnalysisComponent } from './analysis/analysis.component';
-
+import { DesignComponent } from './design/design.component';
+import { Routes, RouterModule } from '@angular/router';
+export const DESIGN_ROUTES: Routes = [
+  {
+      path: "", component: DesignComponent, children: [
+          
+         
+          { path: "wireframe", component: WireframeComponent },
+          { path: "ui", component: UxComponent },
+          { path: "analysis", component: AnalysisComponent },
+          
+      
+      ]
+  },
+];
 
 
 @NgModule({
-  declarations: [WireframeComponent, UxComponent, AnalysisComponent],
+  declarations: [WireframeComponent, UxComponent, AnalysisComponent, DesignComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(
+      DESIGN_ROUTES
+  ),
   ]
 })
 export class DesignModule { }
