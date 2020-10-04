f7f00a10c685d3821469aa777719c1284a5b408f
Gopi Maruvada <gopi.maruvada@gmail.com>
2020-10-04 10:36:23 +0530

    added routing
    added routing

--- src/app/product-development/product-development.module.ts
+++ src/app/product-development/product-development.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 import { SetupComponent } from './setup/setup.component';
 import { ContinuosDeliveryComponent } from './continuos-delivery/continuos-delivery.component';
-
+import { ProductdevelopmentComponent } from './product-development/product-development';
+export const DEV_ROUTES: Routes = [
+  {
+      path: "", component: ProductdevelopmentComponent, children: [
+          
+         
+          { path: "continous-delivery", component: ContinuosDeliveryComponent },
+          { path: "setup", component: SetupComponent },    
+          
+      
+      ]
+  },
+];
 
 
 @NgModule({
-  declarations: [SetupComponent, ContinuosDeliveryComponent],
+  declarations: [SetupComponent, ContinuosDeliveryComponent , ProductdevelopmentComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(
+      DEV_ROUTES
+  ),
   ]
 })
 export class ProductDevelopmentModule { }
---
+++ src/app/product-development/product-development/product-development.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-development',
+  templateUrl: './product-development.html',
+  
+})
+export class ProductdevelopmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
